f6067d47d78fb37a90621c0856667793d9689c4e
ductien.dangnguyen <ductien.dangnguyen@gmail.com>
2021-09-13 12:51:19 +0200

    Finish core classes

--- src/main/java/no/kristiania/http/HTTPClient.java
+++ src/main/java/no/kristiania/http/HTTPClient.java
@@ -2,28 +2,49 @@ package no.kristiania.http;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HTTPClient {
-    private int statusCode;
+    private final int statusCode;
+    private final Map<String, String> headerFields = new HashMap<>();
+    private String messageBody;
 
-    public HTTPClient(String hostname, int port, String requestTarget) throws IOException {
-        Socket socket = new Socket(hostname, port);
-        String request = "GET " + requestTarget + " HTTP/1.1\r\n" +
-                "Host: " + hostname + "\r\n\r\n";
+    public HTTPClient(String host, int port, String requestTarget) throws IOException {
+        Socket socket = new Socket(host, port);
 
-        socket.getOutputStream().write(request.getBytes());
+        String clientRequest =
+                "GET " + requestTarget + " HTTP/1.1\r\n"
+                        + "Host: " + host + "\r\n"
+                        + "\r\n";
 
-        int c;
-        while ((c = socket.getInputStream().read()) != -1) {
-            System.out.print((char) c);
+        socket.getOutputStream().write(clientRequest.getBytes());
+
+        String statusLine = HttpMessage.readLine(socket);
+        this.statusCode = Integer.parseInt(statusLine.split(" ")[1]);
+        String headerLine;
+        while (!(headerLine = HttpMessage.readLine(socket)).isBlank()) {
+            int colonPos = headerLine.indexOf(':');
+            String headerName = headerLine.substring(0, colonPos);
+            String headerValue = headerLine.substring(colonPos+1).trim();
+            headerFields.put(headerName, headerValue);
         }
-    }
-
-    public static void main(String[] args) throws IOException {
-        new HTTPClient("httpbin.org", 80, "/html");
+        messageBody = HttpMessage.readBytes(socket, getContentLength());
     }
 
     public int getStatusCode() {
         return statusCode;
+    }
+
+    public String getHeader(String fieldName) {
+        return headerFields.get(fieldName);
+    }
+
+    public int getContentLength() {
+        return Integer.parseInt(getHeader("Content-Length"));
+    }
+
+    public String getMessageBody() {
+        return messageBody;
     }
 }
